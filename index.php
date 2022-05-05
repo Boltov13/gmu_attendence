1d655b43b5aa68adb2dcd73d736b9138ab7f403f
boltov13 <bolt_1914@mail.ru>
2022-05-05 23:50:27 +0300

    first ommit

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <title>Посещаемость курсанта!</title>
+    <title>Проверить посещаемость</title>
 </head>
 <body>
     <div class="container mt-4">
         <form method="post" action="vendor/seekUserByName">
             <p>Проверка посещаемости курсанта</p>
             <label>
-                <input type="text" name="seek_secondname" placeholder="Введите фамилию">
+                <input type="text" name="seekUserSecondname" placeholder="Введите фамилию">
             </label>
             <label>
-                <input type="text" name="seek_firstname" placeholder="Введите имя">
+                <input type="text" name="seekUserFirstname" placeholder="Введите имя">
             </label>
             <label>
-                <input type="text" name="seek_thirdname" placeholder="Введите отчество">
+                <input type="text" name="seekUserThirdname" placeholder="Введите отчество">
             </label>
             <button type="submit" class="btn btn-success">Найти</button>
         </form>
