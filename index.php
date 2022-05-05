0f0be843e1b8621dbb2d984818b10cdcb4fdf8bf
boltov13 <bolt_1914@mail.ru>
2022-05-05 22:03:56 +0300

    init

---
+++ index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <title>Посещаемость курсанта!</title>
+</head>
