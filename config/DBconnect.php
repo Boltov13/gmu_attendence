5e711052c242c5ec517d09e4cc98fb679e6e9e3f
boltov13 <bolt_1914@mail.ru>
2022-05-08 13:16:34 +0300

    database connected, seek userbyname page modified

--- config/DBconnect.php
+++ config/DBconnect.php
@@ -1,12 +1,16 @@
 <?php
-$servername = "localhost";
-$database = "gmudatabase";
-$username = "boltov";
-$password = "Olesya29!";
+function DBconnect(): mysqli
+{
+    $servername = "localhost";
+    $database = "gmudatabase";
+    $username = "boltov";
+    $password = "Olesya29!";
 #Создаем соединение
-$connection = mysqli_connect($servername, $username, $password, $database);
+    $connection = mysqli_connect($servername, $username, $password, $database);
 #Проверяем соединение
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully!\n";
+    return $connection;
 }
-echo "Connected successfully!\n";
--- vendor/seekUserByname.php
+++ vendor/seekUserByname.php
@@ -1,10 +1,16 @@
 <?php
-include 'config/DBconnect.php';
-
-$firstname_seek = $_POST['seek_firstname'];
-$secondname_seek = $_POST['seek_secondname'];
-$thirdname_seek = $_POST['seek_thirdname'];
-$seek_result = mysqli_query($connection, query:"SELECT * FROM `users` WHERE `first_name` LIKE '$firstname_seek' AND `second_name` LIKE '$secondname_seek' AND `third_name` LIKE '$thirdname_seek'");
+include '../config/DBconnect.php';
+$seek_result = mysqli_query(DBconnect(), query: "SELECT * FROM users WHERE first_name='Илья' AND second_name='Товстяк'");
 var_dump($seek_result);
-$firstname_result = mysqli_fetch_all($seek_result);
-var_dump($seek_result);
+while ($row = $seek_result->fetch_array()){$the_rows[] = $row;}
+foreach ($the_rows as $row)
+{
+    echo $row[0];
+    echo $row[1];
+    echo $row[2];
+    echo $row[3];
+    echo $row[4];
+    echo $row[5];
+    echo $row[6];
+    echo $row[7];
+}
